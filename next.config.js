eb13850d62246e9824bd3cfb99c12be483ffa0fc
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-01-22 13:43:46 +0900

    next.config.jsと環境変数を修正

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
 module.exports = {
   env: {
-    WEATHER_API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
+    WEATHER_API_KEY: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
   },
-};
-module.exports = {
+
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
   },
 };
